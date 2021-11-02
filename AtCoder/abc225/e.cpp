87ff6ee21a99fb0d3da4fda0fb3327eeb8dd1835
Kitamura <ymyk6602@gmail.com>
2021-11-02 22:52:28 +0900

    practice: abc225

--- AtCoder/abc225/e.cpp
+++ AtCoder/abc225/e.cpp
@@ -6,60 +6,46 @@
  */
 #include <bits/stdc++.h>
 
+struct Fraction {
+    long long int x, y;
+    Fraction() {}
+    Fraction(int x, int y) : x(x), y(y) {}
+};
+
+bool operator>(Fraction& lhs, Fraction& rhs) { return lhs.y * rhs.x > lhs.x * rhs.y; }
+bool operator<(Fraction& lhs, Fraction& rhs) { return lhs.y * rhs.x < lhs.x * rhs.y; }
+bool operator==(Fraction& lhs, Fraction& rhs) { return lhs.y * rhs.x == lhs.x * rhs.y; }
+
+
+std::ostream& operator<<(std::ostream& os, const Fraction& f) {
+    os << f.x << ", " << f.y;
+    return os;
+}
 
 int main() {
     int N;
     std::cin >> N;
 
-    std::vector<long long int> max_x(N), max_y(N), min_x(N), min_y(N);
+    std::vector<Fraction> mins(N), maxs(N);
     for (int i = 0; i < N; i++) {
         int x, y;
         std::cin >> x >> y;
-        max_x[i] = x - 1;
-        max_y[i] = y;
-        if ((y - 1) * x > y * (x - 1)) {
-            min_x[i] = x;
-            min_y[i] = y;
-        } else {
-            min_x[i] = x - 1;
-            min_y[i] = y - 1;
-        }
-        // std::cout << (double)y / (x - 1) << std::endl;
+        mins[i] = Fraction(x, y - 1);
+        maxs[i] = Fraction(x - 1, y);
     }
-    std::vector<int> idx(N, 0);
+
+    std::vector<int> idx(N);
     std::iota(idx.begin(), idx.end(), 0);
-    std::sort(idx.begin(), idx.end(), [&max_x, &max_y, &min_x, &min_y](auto& lhs, auto& rhs) {
-        if (max_y[lhs] * max_x[rhs] == max_y[rhs] * max_x[lhs]) {
-            return min_y[lhs] * min_x[rhs] < min_y[rhs] * min_x[lhs];
-        }
-        return max_y[lhs] * max_x[rhs] < max_y[rhs] * max_x[lhs];
-    });
-    int max = 1;
-    long long int mx = max_x[idx[0]];
-    long long int my = max_y[idx[0]];
-    std::vector<int> dp(N, 1);
+    std::sort(idx.begin(), idx.end(), [&maxs](auto& lhs, auto& rhs) { return maxs[lhs] < maxs[rhs]; });
+
+    int cnt = 1;
+    auto cur = maxs[idx[0]];
     for (int i = 1; i < N; i++) {
-        long long int x = min_x[idx[i]];
-        long long int y = min_y[idx[i]];
-        // std::cout << i << " , " << idx[i] << " , " << (double)y / x << std::endl;
-        if (y * mx < x * my) {
+        if (mins[idx[i]] < cur) {
             continue;
         }
-        int imin = 0;
-        int imax = i;
-        while (imin != imax) {
-            int mid = (imin + imax) / 2;
-            // std::cout << (double)y / x << " , " << (double)max_y[idx[mid]] / max_x[idx[mid]]
-            //           << std::endl;
-            if (y * max_x[idx[mid]] >= x * max_y[idx[mid]]) {
-                imin = mid + 1;
-            } else {
-                imax = mid;
-            }
-        }
-        // std::cout << imin << std::endl;
-        dp[i] = dp[imin - 1] + 1;
-        max = std::max(dp[i], max);
+        cnt++;
+        cur = maxs[idx[i]];
     }
-    std::cout << max << std::endl;
+    std::cout << cnt << std::endl;
 }
